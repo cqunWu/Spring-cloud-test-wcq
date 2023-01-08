a6735b1199254d7736effbf59cda311482596c49
cqunWu <1289544990@qq.com>
2023-01-08 12:52:45 +0800

    Eureka服务

---
+++ src/main/java/com/eureka/wcq/EurekaServerApplication.java
@@ -0,0 +1,20 @@
+package com.eureka.wcq;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author: Chaoqun Wu
+ * @description Eureka服务端启动类
+ * @date: 2023/1/8 12:25
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(EurekaServerApplication.class).web(WebApplicationType.NONE).run(args);
+
+    }
+}
